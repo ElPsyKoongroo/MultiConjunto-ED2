71ca35d447eef92dd313cb74e475a9fb698fd671
Adrian <arrodriguez1809@gmail.com>
2022-03-15 12:08:26 +0100

    Complete

--- src/main1.cpp
+++ src/main1.cpp
@@ -11,47 +11,59 @@ int main()
     std::cout << "\nPrueba M1, INT\n\n";
 
 #pragma region M1
+
     for (int i = 1; i < 20; i++)
     {
-        std::cout << m1.cardinalidad() << std::endl;
+        std::cout << "Se incluye: " << i << std::endl;
         m1.anade(i);
+        std::cout << "Hay " << m1.cardinalidad() << " elementos" << std::endl;
     }
 
-    std::cout << m1.cardinalidad() << std::endl;
+    std::cout << "En total al final hay: " << m1.cardinalidad() << " elementos" << std::endl;
+
+    for(int i = 0; i < m1.cardinalidad()+10; i+=3)
+    {
+        std::cout << "El elemento (" << i << ")" << (m1.pertenece(i)? " ":" no ") << "pertenece al conjunto" << std::endl;
+    }
 
     for(int i = 0; i < 20; i++)
     {
+        std::cout << "Se elimina: " << i << std::endl;
         m1.elimina(i);
         std::cout << "Quedan: " << m1.cardinalidad() << std::endl;
     }
-    for(int i = 0; i < m1.cardinalidad(); i+=3)
-    {
-        std::cout << "El elemento (" << i << ")" << (m1.pertenece(i)? " ":" no") << "pertenece al conjunto" << std::endl;
-    }
 #pragma endregion
 
+    system("pause");
+    system("cls");
     std::cout << "\n\nPrueba M2, CHAR\n\n";
 
 #pragma region M2
-    for (int i = 1; i < 20; i++)
+
+    for (char i = 65; i < 91; i++)
     {
-        std::cout << m2.cardinalidad() << std::endl;
+        std::cout << "Se incluye: " << i << std::endl;
         m2.anade(i);
+        std::cout << "Hay " << m2.cardinalidad() << " elementos" << std::endl;
     }
 
-    std::cout << m2.cardinalidad() << std::endl;
+    std::cout << "En total hay " << m2.cardinalidad() << " elementos" << std::endl;
 
-    for(int i = 0; i < 20; i++)
+    for(char i = 65; i < 100; i+=3)
     {
+        std::cout << "El elemento (" << i << ")" << (m2.pertenece(i)? " ":" no ") << "pertenece al conjunto" << std::endl;
+    }
+
+    for(char i = 65; i < 91; i++)
+    {
+        std::cout << "Se elimina: " << i << std::endl;
         m2.elimina(i);
         std::cout << "Quedan: " << m2.cardinalidad() << std::endl;
     }
-    for(int i = 0; i < m2.cardinalidad(); i+=3)
-    {
-        std::cout << "El elemento (" << i << ")" << (m2.pertenece(i)? " ":" no") << "pertenece al conjunto" << std::endl;
-    }
 #pragma endregion
-
+    
+    system("pause");
+    system("cls");
     std::cout << "\n\nPrueba 3, PERSONA\n\n";
 
 #pragma region M3
@@ -63,19 +75,26 @@ int main()
     Persona p2 = Persona(nombres[1], 19);
     Persona p3 = Persona(nombres[2], 21);
 
+    std::cout << "Se incluyen tres personas, p1, p2 y p3\n\n";
     m3.anade(p1);
     m3.anade(p2);
     m3.anade(p3);
 
-    std::cout << m3.cardinalidad() << std::endl;
+    std::cout << "Hay " << m3.cardinalidad() << " personas incluidas\n\n" << std::endl;
 
+    //show p1, p2, p3
+    std::cout << "p1: " << p1.getNombre() << " " << p1.getEdad() << std::endl;
+    std::cout << "p2: " << p2.getNombre() << " " << p2.getEdad() << std::endl;
+    std::cout << "p3: " << p3.getNombre() << " " << p3.getEdad() << std::endl;
+
+    std::cout << "Se elimina p1\n\n" << std::endl;
     m3.elimina(p1);
 
-    std::cout << m3.cardinalidad() << std::endl;
+    std::cout << "Ahora hay " << m3.cardinalidad() << " personas incluidas\n\n" << std::endl;
 
-    std::cout << (m3.pertenece(p1)? "":"no ") << "pertenece al conjunto" << std::endl;
+    std::cout << p1.getNombre() << " - " << p1.getEdad() << (m3.pertenece(p1)? " ":" no ") << "pertenece al conjunto\n\n" << std::endl;
 
-    std::cout << (m3.pertenece(p2)? "":"no ") << "pertenece al conjunto" << std::endl;
+    std::cout << p2.getNombre() << " - " << p2.getEdad() << (m3.pertenece(p2)? " ":" no ") << "pertenece al conjunto\n\n" << std::endl;
 
 #pragma endregion
 
